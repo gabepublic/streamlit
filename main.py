1d02b091a098641a214ff5e14ecde6b3f90f2598
Gabe Public <gabe.public@outlook.com>
2023-11-30 17:15:57 -0800

    change title

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Personal Agent')
+st.title('Streamlit Playground')
 #st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
